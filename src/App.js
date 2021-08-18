a589e4ecb3bfadb084cad958b3222f9192052966
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2021-08-18 20:46:10 +0400

    t

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import React, { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [item, setItem] = useState(1);
+  const minus = () => {
+    if (item != 0) {
+      setItem(item - 1);
+    }
+  };
+
+  return (
+    <div className='App'>  
+      <h1>{item}</h1>
+      <br />
+      <button onClick={() => setItem(item + 1)}> + </button>
+      <div>
+        <button onClick={minus}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
